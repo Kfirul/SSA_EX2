d56756035f48026afc9ef5f2a42520bd05b40c45
Kfir <kfirul14@gmail.com>
2022-11-23 15:51:11 +0200

    add more function

--- my_mat.c
+++ my_mat.c
@@ -1,43 +1,49 @@
-#include<stdio.h>
-#include<math.h>
-#define SIZE 10
+    #include<stdio.h>
+    #include<math.h>
+    #define SIZE 10
 
-int mat[SIZE][SIZE];
+    int mat[SIZE][SIZE];
 
-void floydWarshallAlgorithm(int a[SIZE][SIZE]){
-    
-    for(int k=0; k<SIZE; k++){
-        for(int i=0;i<SIZE;i++){
-            for(int j=0;j<SIZE;j++){
+    void floydWarshallAlgorithm(int a[SIZE][SIZE]){
 
-                if(a[i][k]!=0 && a[k][j]!=0){
-                    if(a[i][j]==0)
-                        a[i][j]=a[i][k]+a[k][j];
+        for(int k=0; k<SIZE; k++){
+            for(int i=0;i<SIZE;i++){
+                for(int j=0;j<SIZE;j++){
 
-                    else a[i][j]=  min(a[i][j],a[i][k]+a[k][j]);
+                    if(a[i][k]!=0 && a[k][j]!=0){
+                        if(a[i][j]==0)
+                            a[i][j]=a[i][k]+a[k][j];
+
+                        else a[i][j]=  min(a[i][j],a[i][k]+a[k][j]);
+                    }
                 }
             }
         }
     }
-}
 
-void makeMat(){
-    int i,j;
-    for(i=0;i<SIZE;i++){
-        for(j=0;j<SIZE;j++){
-            scanf("%d ",&mat[i][j]);
-}  
-  }
+    void makeMat(){
+        int i,j;
+        for(i=0;i<SIZE;i++){
+            for(j=0;j<SIZE;j++){
+                scanf("%d ",&mat[i][j]);
 
-}
+    }  
+    }
+    floydWarshallAlgorithm(mat);
+    }
 
-void isConnect(){
-    int i,j;
-    scanf("%d %d",&i,&j);
-    if(mat[i][j]==0) printf("False\n");
-    else printf("True\n");
-}
+    void isConnect(){
+        int i,j;
+        scanf("%d %d",&i,&j);
+        if(mat[i][j]==0) printf("False\n");
+        else printf("True\n");
+    }
 
-void shortestPath(); 
+    void shortestPath(){
+        int i,j;
+        scanf("%d %d",&i,&j);
+        if(mat[i][j]==0) printf("-1\n");
+        else printf("%d\n",mat[i][j]);
+    }
 
 
